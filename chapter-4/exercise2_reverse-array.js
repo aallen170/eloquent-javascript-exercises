fdd8a5b94e8edd8a5a698c15dbd8125260831874
aallen170 <aallen170@gmail.com>
2019-09-21 11:34:49 -0500

    auto-update

--- chapter-4/exercise2_reverse-array.js
+++ chapter-4/exercise2_reverse-array.js
@@ -1,4 +1,24 @@
+function reverseArray(arr) {
+  let newArr = [];
 
+  for (let i = arr.length - 1; i >= 0; i--) newArr.push(arr[i]);
+
+  return newArr;
+}
+
+function reverseArrayInPlace(arr) {
+  let p1 = 0; p2 = arr.length-1;
+
+  while (p1 < p2) {
+    let tmp = arr[p1];
+    arr[p1] = arr[p2];
+    arr[p2] = tmp;
+    p1++;
+    p2--;
+  }
+  
+  return arr;
+}
 
 console.log(reverseArray(["A", "B", "C"]));
 // → ["C", "B", "A"];
