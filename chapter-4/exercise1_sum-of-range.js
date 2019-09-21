bf40efbcc60f6788d6b30954dfd985df2f1da73b
aallen170 <aallen170@gmail.com>
2019-09-21 11:19:38 -0500

    auto-update

--- chapter-4/exercise1_sum-of-range.js
+++ chapter-4/exercise1_sum-of-range.js
@@ -1,18 +1,32 @@
-function range(start, end, step) {
+// First attempt:
+// function range(start, end, step) {
+//   let arr = [];
+
+//   if (step < 0) {
+//     while (start >= end) {
+//       arr.push(start);
+//       start += step;
+//     }
+//   } else if (step > 0) {
+//     while (start <= end) {
+//       arr.push(start);
+//       start += step;
+//     }
+//   } else if (step === 0) return 'step cannot be 0';
+//   else while (start <= end) arr.push(start++);
+  
+//   return arr;
+// }
+
+
+function range(start, end, step = start < end ? 1 : -1) {
   let arr = [];
 
   if (step < 0) {
-    while (start >= end) {
-      arr.push(start);
-      start += step;
-    }
+    for (let i = start; i >= end; i += step) arr.push(i);
   } else if (step > 0) {
-    while (start <= end) {
-      arr.push(start);
-      start += step;
-    }
-  } else if (step === 0) return 'step cannot be 0';
-  else while (start <= end) arr.push(start++);
+    for (let i = start; i <= end; i += step) arr.push(i);
+  } else return 'step cannot be 0';
   
   return arr;
 }
@@ -31,3 +45,30 @@ console.log(range(5, 2, -1));
 // → [5, 4, 3, 2]
 console.log(sum(range(1, 10)));
 // → 55
+
+// Solution:
+// function range(start, end, step = start < end ? 1 : -1) {
+//   let array = [];
+
+//   if (step > 0) {
+//     for (let i = start; i <= end; i += step) array.push(i);
+//   } else {
+//     for (let i = start; i >= end; i += step) array.push(i);
+//   }
+//   return array;
+// }
+
+// function sum(array) {
+//   let total = 0;
+//   for (let value of array) {
+//     total += value;
+//   }
+//   return total;
+// }
+
+// console.log(range(1, 10))
+// // → [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// console.log(range(5, 2, -1));
+// // → [5, 4, 3, 2]
+// console.log(sum(range(1, 10)));
+// // → 55
