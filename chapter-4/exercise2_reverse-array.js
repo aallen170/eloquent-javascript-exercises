d3325f15affb68d2b5d95bfec58df9bda66c6149
aallen170 <aallen170@gmail.com>
2019-09-21 11:39:52 -0500

    auto-update

--- chapter-4/exercise2_reverse-array.js
+++ chapter-4/exercise2_reverse-array.js
@@ -23,6 +23,31 @@ function reverseArrayInPlace(arr) {
 console.log(reverseArray(["A", "B", "C"]));
 // → ["C", "B", "A"];
 let arrayValue = [1, 2, 3, 4, 5];
+console.log(reverseArrayInPlace(arrayValue));
+// → [5, 4, 3, 2, 1]
+//--------------------
+
+// Solution:
+function reverseArray(array) {
+  let output = [];
+  for (let i = array.length - 1; i >= 0; i--) {
+    output.push(array[i]);
+  }
+  return output;
+}
+
+function reverseArrayInPlace(array) {
+  for (let i = 0; i < Math.floor(array.length / 2); i++) {
+    let old = array[i];
+    array[i] = array[array.length - 1 - i];
+    array[array.length - 1 - i] = old;
+  }
+  return array;
+}
+
+console.log(reverseArray(["A", "B", "C"]));
+// → ["C", "B", "A"];
+let arrayValue = [1, 2, 3, 4, 5];
 reverseArrayInPlace(arrayValue);
 console.log(arrayValue);
 // → [5, 4, 3, 2, 1]
