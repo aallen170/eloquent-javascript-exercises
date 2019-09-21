122681931af101fe999adeb7b361c19392460357
aallen170 <aallen170@gmail.com>
2019-09-20 21:04:40 -0500

    auto-update

--- chapter-2/exercise3_chessboard.js
+++ chapter-2/exercise3_chessboard.js
@@ -16,3 +16,21 @@ for (let i = 0; i < size; i++) {
   console.log(str);
   alternate = !alternate;
 }
+
+// Solution:
+let size = 8;
+
+let board = "";
+
+for (let y = 0; y < size; y++) {
+  for (let x = 0; x < size; x++) {
+    if ((x + y) % 2 == 0) {
+      board += " ";
+    } else {
+      board += "#";
+    }
+  }
+  board += "\n";
+}
+
+console.log(board);
